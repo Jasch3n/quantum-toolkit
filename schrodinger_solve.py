0dc6d35e0473fbbe4d4d6a2deb8768fe06a003ea
jaspaaaaaa <jasper20020202@gmail.com>
2020-11-08 22:31:19 -0800

    Graphing Utilities

--- schrodinger_solve.py
+++ schrodinger_solve.py
@@ -2,7 +2,8 @@ import numpy as np
 from numpy import polyval
 from scipy.special import genlaguerre
 import matplotlib.pyplot as plt
-
+from matplotlib.animation import FuncAnimation
+from matplotlib import animation
 
 class SchrodingerEquation:
     """
@@ -17,7 +18,7 @@ class SchrodingerEquation:
         self.discrete_potential_matrix = np.diag(self.potential_values)
         self.dimension = intervals
         self.ddx2_matrix = self.ddx2(intervals)
-        self.hamiltonian = - (hbar**2 / (2 * m)) * self.ddx2_matrix + self.discrete_potential_matrix
+        self.hamiltonian = - (hbar ** 2 / (2 * m)) * self.ddx2_matrix + self.discrete_potential_matrix
         self.eigensolution = self.solve_eigenproblem()
         #todo
 
@@ -33,27 +34,138 @@ class SchrodingerEquation:
     def solve_eigenproblem(self):
         """
             uses the discrete Hamiltonian matrix obtained from the constructor
-            to compute the eigenvectors and eigenvalues of it
+            to compute the eigenvectors and eigenvalues of it and sort them by
+            ascending order
         """
         e_vals, e_vecs = np.linalg.eig(self.hamiltonian)
         sorted_e_vals_indices = np.argsort(e_vals)
         return e_vals[sorted_e_vals_indices], e_vecs[:, sorted_e_vals_indices]
+
+    def graph_nth_eigenstate(self, n):
+        """
+            Given an integer n, graphs the n-th energy eigenstate
+            along with the potential
+
+        Args:
+            n ([int]): [index of energy eigenstate, with 1 being the first]
+        """
+        plt.figure()
+        plt.xlabel("$x$")
+        plt.ylabel("$\psi(x)$")
+        title = "Wavefunciton vs. Position"
+        plt.title(title)
+        plt.plot(self.x_values, self.eigensolution[1][:, n - 1], label="$\psi_{"+ str(n) + "}$")
+        plt.plot(self.x_values, self.potential_values/np.linalg.norm(self.potential_values), label="V(x)")
+        plt.legend()
     
-    def superimpose(self, states):
+    def graph_superimposed(self, states):
         """
             takes in an array of states to superimpose, and returns a vector 
             that approximates the (normalized) superimposed state
-
             states is an array of length at least one
         """
-        ans = np.zeros(self.dimension)
+        plt.figure()
+        plt.title("Superimposed State vs. Position")
+        superimposed = np.zeros(self.dimension)
         for i in states:
-            ans += self.eigensolution[1][:, i - 1]
-        return ans/np.linalg.norm(ans)
+            superimposed += self.eigensolution[1][:, i - 1]
+        label = ""
+        length = len(states)
+        for i in range(length):
+            if i == length - 1: 
+                label += "$\psi_" + str(states[i]) + "$"
+            else:
+                label += "$\psi_" + str(states[i]) + "$ +"
+        plt.plot(self.x_values, superimposed/np.linalg.norm(superimposed), label = label)
+        plt.plot(self.x_values, self.potential_values/np.linalg.norm(self.potential_values), label="V(x)")
+
+    def graph_nth_prob_density(self, n):
+        """
+            takes in a state index and graphs its probability density against position
+
+        Args:
+            n (int): Index of the State to be graphed
+        """
+        plt.figure()
+        plt.xlabel("$x$")
+        plt.ylabel("$|\psi(x)|^2$")
+        title = "Probability Density vs. Position"
+        plt.title(title)
+        plt.plot(self.x_values, self.eigensolution[1][:, n - 1] * self.eigensolution[1][:, n - 1], label="$|\psi_{"+ str(n) + "}|^2$")
+        plt.legend()
+
+    def time_evolved_state(self, n, t, hbar=1):
+        """Given a state n and a time t, evaluates the vector approximating the state at time t
+
+        Args:
+            n (int): index of state
+            t (float): time
+        """
+        energy_eigenstate = self.eigensolution[1][:, n - 1]
+        energy_eigenvalue = self.eigensolution[0][n - 1]
+        phase_factor_from_time = np.exp(-1j * energy_eigenvalue / hbar * t)
+        return energy_eigenstate * phase_factor_from_time
+
+    def display_graph(self):
+        plt.show()
 
 
+def time_evolution_anim(ase, n):
+    """[summary]
+
+    Args:
+        ase ([type]): [description]
+        n ([type]): [description]
+        t ([type]): [description]
+    """
+    fig, ax = plt.subplots()
+
+    plt.title("Time Evolution")
+    def anim_init():
+        line.set_data(ase.x_values, ase.eigensolutions[1][n - 1])
+        return line,
+
+    def animate(t):
+        time_scale = 0.01
+        psi_of_t = ase.time_evolved_state(n, t * 0.01)
+        line.set_data(ase.x_values, psi_of_t)
+        return line
+    
+    anim = animation.FuncAnimation(fig, animate, init_func=anim_init,
+                               frames=200, interval=20, blit=True)
+    plt.show()   
 #test code
-test_equation = SchrodingerEquation(lambda x: 20 * np.heaviside(x, 1)) # omega=1, m=1, hbar=1
+test_equation = SchrodingerEquation(lambda x: x ** 2) # omega=1, m=1, hbar=1
+
+# test_equation.graph_nth_eigenstate(10)
+# test_equation.graph_nth_prob_density(10)
+# test_equation.graph_superimposed([1, 2, 3])
+
+def wavefunction_time_evolution_anim(ase, n):
+    """
+        Takes in a Schrodinger Equation datum and an integer,
+        and creates an animation demonstrating the time evolution of the 
+        n-th excited state of the solution to the Schrodinger Equation
+
+        Args:
+            ase (SchrodingerEquation): a SchrodingerEquation datum
+            n (int): the index of the excited state
+    """
+    fig = plt.figure()
+    ax = plt.axes(xlim=(-4, 4), ylim=(-0.1, 0.1))
+    line, = ax.plot([], [], lw=3)
+
+    def init():
+        line.set_data([], [])
+        return line,
+    def animate(i):
+        x = np.linspace(-4, 4, 1000)
+        y = ase.time_evolved_state(n, i * 0.01)
+        line.set_data(x, y)
+        return line,
+
+    anim = FuncAnimation(fig, animate, init_func=init, interval=10, blit=True)
+    plt.show()
 
 # print("\nThe discrete apprixmation to V(x):\n", test_equation.discrete_potential_matrix)
 # print("\nThe discrete approximation to ddx2:\n", test_equation.ddx2_matrix)
@@ -64,14 +176,14 @@ test_equation = SchrodingerEquation(lambda x: 20 * np.heaviside(x, 1)) # omega=1
 #     print("\nThe " + str(n) + "th energy eigenvalue for this setup is:", len(test_equation.eigensolution[0][n-1]), "\nThe analytic solution for this setup is:", (n - 1) + 0.5)
 #test_superimposed = test_equation.superimpose([1, 2, 3])
 
-plt.figure()
-plt.xlabel("$x$")
-plt.ylabel("$\psi(x)$")
-plt.title("Wavefunction vs. Position")
-# plt.plot(test_equation.x_values, test_equation.eigensolution[1][:, 4], linestyle="--")
-# plt.plot(test_equation.x_values, test_equation.eigensolution[1][:, 6], linestyle="--")
-plt.plot(test_equation.x_values, test_equation.eigensolution[1][:, 10], linestyle="--")
-plt.plot(test_equation.x_values, test_equation.potential_values/np.linalg.norm(test_equation.potential_values), linestyle=":", label="potential")
-#plt.plot(test_equation.x_values, test_superimposed, label="Superimoposed (0, 1, 2) State")
-plt.legend()
-plt.show()
+# plt.figure()
+# plt.xlabel("$x$")
+# plt.ylabel("$\psi(x)$")
+# plt.title("Wavefunction vs. Position")
+# # plt.plot(test_equation.x_values, test_equation.eigensolution[1][:, 4], linestyle="--")
+# # plt.plot(test_equation.x_values, test_equation.eigensolution[1][:, 6], linestyle="--")
+# plt.plot(test_equation.x_values, test_equation.eigensolution[1][:, 10], linestyle="--")
+# plt.plot(test_equation.x_values, test_equation.potential_values/np.linalg.norm(test_equation.potential_values), linestyle=":", label="potential")
+# #plt.plot(test_equation.x_values, test_superimposed, label="Superimoposed (0, 1, 2) State")
+# plt.legend()
+# plt.show()
--- test.py
+++ test.py
@@ -3,26 +3,34 @@ from numpy import polyval
 from scipy.special import genlaguerre
 import matplotlib.pyplot as plt
 
-def associated_laguerre(alpha, n):
-    """Given alpha and n, return a ordpoly representing 
-        the associated laguerre polynomial L^(alpha)_n
+from schrodinger_solve import *
 
-    Args:
-        alpha ([int]): [> -1]
-        n ([int]): [> -1]
+# def associated_laguerre(alpha, n):
+#     """Given alpha and n, return a ordpoly representing 
+#         the associated laguerre polynomial L^(alpha)_n
 
-    Returns:
-        [ordpoly]: [a numpy polynomial representating an associated laguerre polynomial]
-    """
-    if (alpha < 0) or (n < 0):
-        raise("invalid input for p and q")
-    return genlaguerre(n, alpha)
+#     Args:
+#         alpha ([int]): [> -1]
+#         n ([int]): [> -1]
 
-test_laguerre = associated_laguerre(2, 10)
-domain = np.linspace(-100, 100, 100)
-lag_values = list(map(lambda x: polyval(test_laguerre, x), domain))
+#     Returns:
+#         [ordpoly]: [a numpy polynomial representating an associated laguerre polynomial]
+#     """
+#     if (alpha < 0) or (n < 0):
+#         raise("invalid input for p and q")
+#     return genlaguerre(n, alpha)
 
-plt.figure()
-plt.plot(domain, lag_values)
-plt.plot()
-plt.show()
+# test_laguerre = associated_laguerre(2, 10)
+# domain = np.linspace(-100, 100, 100)
+# lag_values = list(map(lambda x: polyval(test_laguerre, x), domain))
+
+# plt.figure()
+# plt.plot(domain, lag_values)
+# plt.plot()
+# plt.show()
+
+def harmonic_oscillator_pot(x, omega=1):
+    return 0.5 * omega**2 * x ** 2
+
+def free_particle_pot(x):
+    return 0
